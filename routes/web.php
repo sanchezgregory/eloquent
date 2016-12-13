cd7482e2c1cc4a238f359dde99786395df7f2a9d
Gregory <mcgregox@gmail.com>
2016-12-12 20:55:50 -0400

    Eloquent create and update

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,31 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+use App\User;
+
+Route::get('/create-user', function () {
+
+    $user = User::create ([
+        'name' =>  'mkariana Guzman',
+        'email' =>  'marianita.a.g@gmail.com',
+        'password' =>   bcrypt('123456'),
+        'gender' => 'f',
+        'biography' =>  'Propietaria'
+    ]);
+
+    return 'Usuario guardado';
+
+});
+
+Route::get('/update-user', function () {
+
+    $user = User::find(1);
+
+    $user->name='Gregory Sanchez';
+    $user->biography = 'Profesor BD';
+
+    $user->save();
+
+    return 'Usuario actualizado';
+
 });
